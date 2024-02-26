35149d666f3cbe6a9b4964687caf61ed906108dd
koreacat <kj9966@naver.com>
2024-02-26 22:02:16 +0900

    feat: code splitting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import('./pages/ListPage/index'));
+const ViewPage = lazy(() => import('./pages/ViewPage/index'));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ListPage />} exact />
-          <Route path="/view/:id" element={<ViewPage />} exact />
-        </Routes>
+        <Suspense fallback={'loading...'}>
+          <Routes>
+            <Route path="/" element={<ListPage />} exact />
+            <Route path="/view/:id" element={<ViewPage />} exact />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
